aeaa49c4c80d55ba3e3842021a7fe1facfe1b8dc
viriya27447 <viriya27447@gmail.com>
2024-10-02 23:00:50 +0700

    init

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import h5py
+import requests
+import io
 
 # ตั้งชื่อแอป
-st.title("Read H5 File and Display Datasets")
+st.title("Read H5 File from URL and Display Datasets")
 
-# กำหนดที่อยู่ของไฟล์ H5 ที่จะอ่าน
-file_path = 'D:\keras_model.h5'  # เปลี่ยนเป็นชื่อไฟล์ของคุณ
+# URL ของไฟล์ H5
+file_url = 'https://firebasestorage.googleapis.com/v0/b/project-5195649815793865937.appspot.com/o/keras_model.h5?alt=media&token=c75818c8-a1ed-485e-9d57-b934d1743517'  # เปลี่ยนเป็น URL ของไฟล์ H5
 
-# เปิดไฟล์ H5
+# ดาวน์โหลดไฟล์ H5
 try:
-    with h5py.File(file_path, 'r') as h5_file:
+    response = requests.get(file_url)
+    response.raise_for_status()  # ตรวจสอบว่าการดาวน์โหลดสำเร็จ
+
+    # เปิดไฟล์ H5 จากข้อมูลที่ดาวน์โหลด
+    with h5py.File(io.BytesIO(response.content), 'r') as h5_file:
         # แสดงรายการ datasets ในไฟล์
         st.write("Datasets in the H5 file:")
         for dataset in h5_file.keys():
