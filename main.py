7cf4fd20d14770d9503124d2b4d2068787752627
viriya27447 <viriya27447@gmail.com>
2024-10-02 22:41:35 +0700

    init

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import streamlit as st
 import h5py
 
 # ตั้งชื่อแอป
-st.title("Upload H5 File")
+st.title("Read H5 File and Display Datasets")
 
 # อัปโหลดไฟล์ H5
-uploaded_file = st.file_uploader("Choose an H5 file...", type=["h5"])
 
+uploaded_file = '"D:\keras_model.h5"'
 # ถ้ามีการอัปโหลดไฟล์
 if uploaded_file is not None:
     # เปิดไฟล์ H5
@@ -15,3 +15,10 @@ if uploaded_file is not None:
         st.write("Datasets in the H5 file:")
         for dataset in h5_file.keys():
             st.write(dataset)
+
+        # ตัวอย่างการแสดงข้อมูลจาก dataset แรก
+        if h5_file:
+            first_dataset = next(iter(h5_file.keys()))  # ดึงชื่อ dataset แรก
+            data = h5_file[first_dataset][:]
+            st.write(f"Data from dataset '{first_dataset}':")
+            st.write(data)
