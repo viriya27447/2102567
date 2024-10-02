9dcede5deb9c7139bf4a9f1829e3a096cd3f5b48
viriya27447 <viriya27447@gmail.com>
2024-10-02 22:58:46 +0700

    init

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import h5py
 # ตั้งชื่อแอป
 st.title("Read H5 File and Display Datasets")
 
-# อัปโหลดไฟล์ H5
+# กำหนดที่อยู่ของไฟล์ H5 ที่จะอ่าน
+file_path = 'D:\keras_model.h5'  # เปลี่ยนเป็นชื่อไฟล์ของคุณ
 
-file_path = '/content/keras_model.h5'  # เปลี่ยนเป็นชื่อไฟล์ของคุณ
+# เปิดไฟล์ H5
+try:
+    with h5py.File(file_path, 'r') as h5_file:
+        # แสดงรายการ datasets ในไฟล์
+        st.write("Datasets in the H5 file:")
+        for dataset in h5_file.keys():
+            st.write(dataset)
 
-with h5py.File(file_path, 'r') as h5_file:
-    # แสดงรายการ datasets ในไฟล์
-    st.write("Datasets in the H5 file:")
-    for dataset in h5_file.keys():
-        st.write(dataset)
+except Exception as e:
+    st.error(f"An error occurred: {e}")
