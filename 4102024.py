2864e4db91142923759c962d77c56b0cbb731f8a
viriya27447 <viriya27447@gmail.com>
2024-10-04 16:32:44 +0700

    init

--- 4102024.py
+++ 4102024.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-st.title("Webcam Live Stream")
+# ตั้งชื่อให้กับแอป
+st.title("Webcam Stream")
+
+# เรียกใช้งาน webrtc_streamer
 webrtc_streamer(key="example")
