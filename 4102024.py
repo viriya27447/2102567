36192b4f94feb4d1419c88e92d818c0368f42f03
viriya27447 <viriya27447@gmail.com>
2024-10-04 16:17:52 +0700

    init

--- 4102024.py
+++ 4102024.py
@@ -1,11 +1,5 @@
 import streamlit as st
-from jupyter_code import calculate_sum  # import ฟังก์ชันจาก Jupyter
+from streamlit_webrtc import webrtc_streamer
 
-# Streamlit UI
-st.title('Jupyter + Streamlit Integration')
-a = st.number_input('Enter a number', value=0)
-b = st.number_input('Enter another number', value=0)
-
-if st.button('Calculate'):
-    result = calculate_sum(a, b)
-    st.write(f'The result is {result}')
+st.title("Webcam Live Stream")
+webrtc_streamer(key="example")
